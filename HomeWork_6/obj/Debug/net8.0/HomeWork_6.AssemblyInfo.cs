a20032e23a9714cb8f4ce03f0fee5aa2b3d564fb
jeffgulick <jeffgulick@gmail.com>
2024-03-03 21:49:48 -0600

    fix build errors

--- HomeWork_6/obj/Debug/net8.0/HomeWork_6.AssemblyInfo.cs
+++ HomeWork_6/obj/Debug/net8.0/HomeWork_6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork_6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1bde11c0f4f41efbb2568f656cc0c704cbb528")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234b2818127cfd6b851b98cce7572694115259a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork_6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork_6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
