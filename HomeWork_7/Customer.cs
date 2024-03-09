9739a99817e8777901601091d2f546b0b09fadf1
jeffgulick <jeffgulick@gmail.com>
2024-03-08 21:20:10 -0600

    updated

--- HomeWork_7/Customer.cs
+++ HomeWork_7/Customer.cs
@@ -4,17 +4,20 @@ class Customer
     public string cus_name;
     public int cus_age;
 
-    public Customer (int cus_id, string cus_name, int cus_age){
+    public Customer (int cus_id, string cus_name, int cus_age)
+    {
         this.cus_id = cus_id;
         this.cus_name = cus_name;
         this.cus_age = cus_age;
     }
-
-    public void ChangeID(int new_id){
+ 
+    public void ChangeID(int new_id)
+    {
         cus_id = new_id;
     }
 
-    public void PrintCusInfo (){
+    public void PrintCusInfo ()
+    {
         Console.WriteLine($"Customer: {cus_id}, Name: {cus_name}, Age: {cus_age}");
     }
 
