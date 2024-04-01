b289579ee3e3ff6d5e8c86e60ca54065480ae5bc
jeffgulick <jeffgulick@gmail.com>
2024-04-01 17:01:25 -0500

    finished solving homework 8

---
+++ Week_11/HomeWork_8/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HomeWork_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student stu_1 = new Student(111, "Alice");
+        Student stu_2 = new Student(222, "Bob");
+        Student stu_3 = new Student(333, "Cathy");
+        Student stu_4 = new Student(444, "David");
+
+        Student.gradebook.Add(stu_1.studentName!, 4.0);
+        Student.gradebook.Add(stu_2.studentName!, 3.6);
+        Student.gradebook.Add(stu_3.studentName!, 2.5);
+        Student.gradebook.Add(stu_4.studentName!, 1.8);
+
+        if(!Student.gradebook.ContainsKey("Tom"))
+        {
+            Student.gradebook.Add("Tom", 3.3);
+        }
+
+        double averageGrade = Student.CalculateAverageGPA();
+        Console.WriteLine($"The average GPA is: {averageGrade}");
+
+        Student.PrintStudentsAboveAverageGPA(averageGrade);
+
+        // foreach(KeyValuePair<string, double> stu in Student.gradebook)
+        // {
+        //     Console.WriteLine($"{stu.Key},  {stu.Value}");
+        // }
+
+    }
+}
---
+++ Week_11/HomeWork_8/Student.cs
@@ -0,0 +1,38 @@
+﻿namespace HomeWork_8;
+
+public class Student
+{
+    public int studentID { get; private set; }
+    public string? studentName { get; private set; }
+    public static List<Student> studentList = new List<Student>();
+    public static Dictionary<string, double> gradebook = new Dictionary<string, double>();
+
+    public Student(int inputStudentID, string inputStudentName)
+    {
+        studentID = inputStudentID;
+        studentName = inputStudentName;
+        studentList.Add(this);
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Student ID: {studentID}, Student Name: {studentName}");
+    }
+
+    public static double CalculateAverageGPA()
+    {
+        return gradebook.Values.Average();
+    }
+
+    public static void PrintStudentsAboveAverageGPA(double grade)
+    {
+        foreach (Student student in studentList)
+        {
+            if (gradebook[student.studentName!] > grade)
+            {
+                student.PrintInfo();
+            }
+        }
+    }
+
+}
