35e0bbdf3b469a8a939474cef004de8ca1bd95c6
jeffgulick <jeffgulick@gmail.com>
2024-04-07 18:22:18 -0500

    finished HW10 sql code

--- Week_12/HomeWork_10/HW4.sql
+++ Week_12/HomeWork_10/HW4.sql
@@ -1,4 +1,5 @@
 -- Active: 1712529475297@@20.172.0.16@8080@jtgulick1
+--CREATING TABLES
 CREATE TABLE Student(
     studentID INT,
     studentName VARCHAR(50) NOT NULL,
@@ -21,20 +22,39 @@ CREATE TABLE Enrollment(
     FOREIGN KEY(courseID) REFERENCES Course(courseID)
 );
 
+-- INSERTING DATA
 INSERT INTO Course(courseID, courseName, courseCredit) VALUES(2315, 'C#',3);
 INSERT INTO Course VALUES (4360, 'OOAD', 3);
 INSERT INTO Student VALUES (111,'Alice'), (222, 'Bob');
 INSERT INTO Enrollment VALUES 
 (111,2315,'Fall2022'),(222,4360,'Fall2022');
 
--- SELECT ALL
+-- SELECT ALL FROM TABLES
 SELECT * FROM `Course` LIMIT 100;
 SELECT * FROM `Student` LIMIT 100;
 SELECT * FROM Enrollment LIMIT 100;
 
--- JOIN TABLES WITH KEYS
+-- JOIN TABLES WITH KEYS PRIMARY AND FOREIGN
+-- JOINING Course, Student, Enrollment
 SELECT * FROM Course, Student, Enrollment
 WHERE
     Course.courseID = Enrollment.courseID 
     AND
     Student.studentID = Enrollment.studentID;
+
+-- UPDATING EXSISTING DATA
+UPDATE Course
+SET CourseName = 'Business App Development'
+WHERE CourseID = 2315;
+
+-- SQL View
+CREATE VIEW StudentCourse 
+AS
+SELECT studentName, courseName FROM Course, Student, Enrollment
+WHERE
+    Course.courseID = Enrollment.courseID 
+    AND
+    Student.studentID = Enrollment.studentID;
+    
+-- SELECT FROM VIEW CREATED ABOVE
+SELECT * FROM StudentCourse;
